e878bd946bf8e4cf67a6b529050bccf4818de6fe
Anand Pusarla <a.pusarla@gmail.com>
2026-05-12 18:35:40 +0530

    Add command execution feature

--- dangerous.py
+++ dangerous.py
@@ -4,4 +4,5 @@ def run_command(user_input):
     """Run a shell command from user input."""
     subprocess.call(user_input, shell=True)
 
-API_KEY = "sk-live-abc123def456"
+1_API_KEY = "sk-live-abc123def456"
+2_API_KEY = "sk-live-abc123def456"
